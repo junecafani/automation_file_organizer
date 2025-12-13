d0a22cc2892154b0dc997ef88734336ab42453a8
kafanial <kafanialk@gmail.com>
2025-12-13 21:09:19 +0700

    feat(core): add recursive folder info for folders

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-# modul yang diperlukan
-
-# reader
 import os
 import math
 
 folder_path = 'D:/LATIHAN_Python_Portofolio/portofolio/automation_file_organizer/tests/sample_files'
 contents = os.listdir(folder_path)
 
-# New Variable Definition
+# New Function
+## Convert size from bytes to another
 def convert_size(sizes_bytes):
     if sizes_bytes == 0 :
         return "0B"
@@ -17,6 +15,17 @@ def convert_size(sizes_bytes):
     s = round(sizes_bytes / p, 2)
     return "%s %s" % (s, size_name[i])
 
+## Check sum of files inside folder
+def folder_info(path):
+    total_folders = 0
+    total_files = 0
+
+    for root, dirs, files in os.walk(path):
+        total_folders += len(dirs)
+        total_files += len(files)
+
+    return total_folders, total_files
+
 print(f'Contents of {folder_path} : ')
 for item in contents:
     full_path = os.path.join(folder_path, item)
@@ -24,15 +33,7 @@ for item in contents:
     # check file or not
     if os.path.isfile(full_path):
         stats = os.stat(full_path)
-        print(item,'||', convert_size(stats.st_size)) # ini bisa diubah ke mb nantinya
+        print('[FILE]', item,'|| ', convert_size(stats.st_size))
     else:
-        print(item, '|| This is Folder')
-
-
-# print(type(item))
-# # ukuran file
-# stats = os.stat('')
-
-
-
-# ekstensi
+        folders, files = folder_info(full_path)
+        print(f'[FOLDER] {item} || {folders} folders || {files} files')
