87581f4a8357c63c497920f9f3d57c7e1d8c71dd
Kafanial Khafi <99129898+junecafani@users.noreply.github.com>
2025-12-13 07:39:19 +0700

    main v0.2.py
    - convert bytes from stats.st_size to different size

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-# modul yang diperlukan
-
-# reader
 import os
+import math
 
-folder_path = 'C:/Users/Cafani/Downloads'
+folder_path = 'D:/LATIHAN_Python_Portofolio/portofolio/automation_file_organizer/tests/sample_files'
 contents = os.listdir(folder_path)
+
+# New Variable Definition
+## Convert bytes from stats.st_size to different size
+def convert_size(sizes_bytes):
+    if sizes_bytes == 0 :
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(sizes_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(sizes_bytes / p, 2)
+    return "%s %s" % (s, size_name[i])
 
 print(f'Contents of {folder_path} : ')
 for item in contents:
     full_path = os.path.join(folder_path, item)
 
-    stats = os.stat(full_path)
-    print(item,'||', stats.st_size, 'bytes') # ini bisa diubah ke mb nantinya
+    # check file or not
+    if os.path.isfile(full_path):
+        stats = os.stat(full_path)
+        print(item,'||', convert_size(stats.st_size)) 
+    else:
+        print(item, '|| This is Folder')
+
 
 # print(type(item))
 # # ukuran file
@@ -20,3 +34,5 @@ for item in contents:
 
 
 # ekstensi
+
+
